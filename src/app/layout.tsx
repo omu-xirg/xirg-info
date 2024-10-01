633cb0e8c6d3ae8e1451dd26cbd45afa68eb4af5
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2024-10-01 19:18:48 +0900

    Rename the URL from nexable.io to ncl.dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.scss'
 export const metadata: Metadata = {
   title: 'Nexable Computing Lab / 融能計算研究グループ',
   description:
-    '大阪公立大学大学院 情報学研究科 融能計算研究グループ (Nexable Computing Lab, 融能研) は、人や道具が持つ能力の相互作用をコンピュータ制御することで社会全体のできることを増やす「融能計算」の研究を行っています。2024年秋の開設に向けて現在準備中です。',
+    '大阪公立大学大学院 情報学研究科 融能計算研究グループ (Nexable Computing Lab, NCL) は、人や道具が持つ能力の相互作用をコンピュータ制御することで社会全体のできることを増やす「融能計算」の研究を行っています。2024年10月に開設された新しい研究室です。',
 }
 
 export default function RootLayout({
